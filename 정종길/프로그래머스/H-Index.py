facdb7056d47d695ff8c94faf7d9824e814fdc1c
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-03-19 22:27:45 +0900

    3월 4주차 -2

---
+++ 정종길/프로그래머스/H-Index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+
+    for i in range(len(citations)):
+        if len(citations[i:]) == citations[i]:
+            answer = citations[i]
+            break
+
+        elif len(citations[i:]) < citations[i]:
+            answer = len(citations[i:])
+            break
+
+    return answer
