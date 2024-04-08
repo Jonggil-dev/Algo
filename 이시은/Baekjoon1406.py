9272e5429a575136f896a2c01e7317941bf2bfea
dorosieun-lee <dorothy.lee.723@gmail.com>
2024-04-09 00:16:41 +0900

    4월 2주차-1

---
+++ 이시은/Baekjoon1406.py
@@ -0,0 +1,20 @@
+# Baekjoon 1406 에디터
+
+from sys import stdin
+
+left = list(input())
+right = []
+
+for _ in range(int(input())):
+    command = list(stdin.readline().split())
+    if command[0] == 'L' and left:
+        right.append(left.pop())
+    elif command[0] == 'D' and right:
+        left.append(right.pop())
+    elif command[0] == 'B' and left:
+        left.pop()
+    elif command[0] == 'P':
+        left.append(command[1])
+
+answer = left + right[::-1]
+print(''.join(answer))
