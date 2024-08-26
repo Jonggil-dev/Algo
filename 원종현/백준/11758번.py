151aa1bcd6eb853a318f161adebc28a1d670e496
www-jong <weon1009@gmail.com>
2024-08-26 14:42:10 +0900

    240826

---
+++ 원종현/백준/11758번.py
@@ -0,0 +1,7 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+val=(x2-x1)*(y3-y1)-(y2-y1)*(x3-x1)
+if val>0:print(1)
+elif val<0:print(-1)
+else:print(0)
