81388bbf7deaa7cba504847495cd77fb8006b777
www-jong <weon1009@gmail.com>
2024-10-24 16:34:46 +0900

    241024

---
+++ 원종현/백준/5582번.py
@@ -0,0 +1,10 @@
+A=input()
+B=input()
+dp=[[0]*(len(B)+1) for _ in range(len(A)+1)]
+res=0
+for i in range(1,len(A)+1):
+    for j in range(1,len(B)+1):
+        if A[i-1]==B[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+            res=max(res,dp[i][j])
+print(res)
