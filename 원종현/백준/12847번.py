13292cd002e43210e73238ffc1e90be78c9a4908
원종 <weon1009@gmail.com>
2025-04-30 11:38:39 +0900

    250430

--- 원종현/백준/12847번.py
+++ 원종현/백준/12847번.py
@@ -1,11 +1,17 @@
 import sys
 input=sys.stdin.readline
-
-N,M=map(int,input().split())
-li=list(map(int,input().split()))
-
-tmp=[0]*(N+1)
-for i in range(1,N+1):
-    tmp[i]=tmp[i-1]+li[i-1]
-res=[tmp[i]-tmp[i-M] for i in range(M,N)]
-print(max(res))
+M=int(input())
+tot=0
+xor=0
+for i in range(M):
+    li=list(map(int,input().split()))
+    if li[0]==1:
+        tot+=li[1]
+        xor=xor^li[1]
+    elif li[0]==2:
+        tot-=li[1]
+        xor=xor^li[1]
+    elif li[0]==3:
+        print(tot)
+    elif li[0]==4:
+        print(xor)
