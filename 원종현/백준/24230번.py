0cfe09619b633b3b449a933e6dd374ee62849b0f
원종 <weon1009@gmail.com>
2025-09-01 19:43:36 +0900

    250901

---
+++ 원종현/백준/24230번.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+
+def func(fir,next):
+    global cnt
+    visit[next]=1
+    if li[fir]!=li[next]:
+        cnt+=1
+    for i in graph[next]:
+        if not visit[i]:
+            func(next,i)
+
+N=int(input())
+li=[0]+list(map(int,input().split()))
+graph=[[] for _ in range(N+1)]
+for _ in range(N-1):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+cnt=0
+visit=[0]*(N+1)
+
+func(0,1)
+print(cnt)
